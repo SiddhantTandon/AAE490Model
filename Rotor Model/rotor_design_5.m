94f2c5529606f87c48e0b35a399ccac95730fe8a
noahmarquand <37167745+noahmarquand@users.noreply.github.com>
2018-03-21 18:33:13 -0400

    Update rotor_design_5.m
    Updated theta calculation to ideal twist, updated mass calculation to average thickness, as well as additional small changes

--- Rotor Model/rotor_design_5.m
+++ Rotor Model/rotor_design_5.m
@@ -5,17 +5,16 @@ airfoil = dlmread('NACA 4404 Data.txt');
 airfoil = airfoil';
  
 % Input Variables
-sections = 6;
+sections = 5;
 vehicle_mass = 60; % kg
-mass_max = .85; % kg
-P_max = 1000; % W
+mass_max = 1; % kg
  
 % Constants
 rho = .02; %kg/m^3
 a = 240; %m/s
 mu = 1.422*10^-5; %kg/m/s
 material_rho = 1600; %kg/m^3    effective density of material taking empty space into account
-thickness = .04; %x/c    average thickness of airfoil
+thickness = .02; %x/c    average thickness of airfoil
 g = 3.71; % m/s^2
  
 weight = vehicle_mass * g;
@@ -28,7 +27,7 @@ thrust_per_blade = thrust_per_rotor/blade_number;
 base_cs = dec2base(0:sections^sections-1,sections) - '0';
 base_cs = base_cs + 1;
 base_cs = base_cs';
-alphas = zeros(1,sections); % angle of attack
+thetas = zeros(1,sections); % angle of attack
 cls = zeros(1,sections); % coefficient of lift
 cds = zeros(1,sections); % coefficient of drag
 cms = zeros(1,sections); % coefficient of moment
@@ -41,89 +40,110 @@ Res = zeros(1,sections); % Reynolds number
 ts = zeros(1,sections); % thickness
 masses = zeros(1,sections); % mass
 inertias = zeros(1,sections); % moment of inertia
-powers = zeros(1,sections); % required power
 
 success_blades = [];
+fail_check = [0,0,0];
  
 z = 0;
-for r = 0.3:0.05:0.7
+for r = 0.7 % 0.5:0.05:0.7
     r_min = 0.1 * r;
     cs = (r/2)/sections * base_cs;
     A_disk = pi*r^2;
     vi = sqrt(thrust_per_rotor / (2*rho*A_disk));
-    for Mach = 0.5:0.05:0.8
+    for Mach = 0.7 % 0.6:0.05:0.8
         v_tip = Mach * a;
+        CT = thrust_per_rotor / (0.5 * rho * v_tip^2 * A_disk * sqrt(1-Mach^2));
         for c = cs
-            count = 1;
-            while count <= sections
+            for count = 1:sections
                 As(count) = c(count)*(r-r_min)/sections;
                 ts(count) = c(count)*thickness;
-                masses(count) = As(count).*ts(count)*material_rho;
+                masses(count) = As(count)*ts(count)*material_rho;
                 inertias(count) = masses(count)*(count-.5)/sections;
                 count = count + 1;
             end
-            if sum(masses) > mass_max
+            mass = sum(masses);
+            if mass > mass_max
+                fail_check(1) = fail_check(1) + 1;
                 continue
             end
-            count = 1;
-            while count <= sections % iterate through sections
-                v_inf = (r_min + (count - .5)*(r-r_min)/sections)/r*v_tip; % average velocity of section
+            
+            A = sum(As); % total area
+            solidity = blade_number * A / A_disk;
+            theta_tip = 2 * CT / (solidity * 5.7) + 0.5 * sqrt(CT);
+            
+            success = 1;
+            for count = 1:sections % iterate through sections
+                r_pos = r_min + (count - .5)*(r-r_min)/sections;
+                v_inf = r_pos/r*v_tip; % average velocity of section
                 phi = atan(vi/v_inf);
                 v = vi/sin(phi);
                 Re = v*c(count)*rho/mu; % average Reynolds of section
                 if Re < 500
                     Re = 1000;
                 end
-                round(Re,-3);
-                maxclcd = 0;
-                y = 1;
-                n = 1;
+                Re = round(Re,-3);
+                
+                % Calculate ideal twisted position
+                if r_pos < (0.4*r)
+                    theta = theta_tip / 0.4;
+                else
+                    theta = theta_tip * r / r_pos;
+                end
+                
+                % Determine angle of attack
+                alfa = theta - phi;
+                
+                % Find conditions at ideal twist and observed Reynolds number
+                index = 1;
                 for x = airfoil % iterate through data
-                    if x(2) < Re+1 && x(2) > Re-1 % find max cl/cd at Reynolds number
-                        clcd = x(3)/x(4);
-                        if clcd > maxclcd
-                            maxclcd = clcd;
-                            n = y;
-                        end
+                    if x(2) == Re && x(1) == round(alfa*2)/2;
+                        break
                     end
-                    y = y + 1;
+                    index = index + 1;
+                end
+                if index > size(airfoil,2)
+                    success = 0;
+                    fail_check(2) = fail_check(2) + 1;
+                    break
                 end
 
-                % Calculations for individual section of blade
-                alphas(count) = airfoil(1,n);
-                cls(count) = airfoil(3,n);
-                cds(count) = airfoil(4,n);
-                cms(count) = airfoil(6,n);
+                % Extract data for individual section of blade
+                thetas(count) = theta;
+                alfas(count) = alfa;
+                cls(count) = airfoil(3,index)/sqrt(1-Mach^2);
+                cds(count) = airfoil(4,index)/sqrt(1-Mach^2);
+                cms(count) = airfoil(6,index)/sqrt(1-Mach^2);
                 Ls(count) = cls(count)*.5*rho*v^2*c(count)*(r-r_min)/sections;
                 Ds(count) = cds(count)*.5*rho*v^2*c(count)*(r-r_min)/sections;
                 Ms_pitch(count) = cms(count)*.5*rho*v^2*c(count)^2*(r-r_min)/sections;
                 Ms_drag(count) = Ds(count)*(count-.5)/sections;
-                count = count + 1;
+            end
+            if ~success
+                continue
             end
             % Converting individual sections into totals
             L = sum(Ls); % total lift
             D = sum(Ds); % 2D drag
-            A = sum(As); % total area
             AR = r^2/A; % aspect ratio
             Di = (sum(cls)/sections)^2/pi/AR; % minimum induced drag
             D_total = D + Di; % total drag
-            P = (L + D_total)^1.5/(2*rho*A_disk)^.5;
             if L > thrust_per_blade && P < P_max
                 z = z + 1;
-                solidity = A / A_disk;
                 cd = mean(cds);
                 M_pitch = sum(Ms_pitch); % pitching moment
                 M_drag = sum(Ms_drag); % moment from drag
                 inertia = sum(inertias); % moment of inertia of blade
                 % Store data
-                storage.chords = c;
-                storage.angles = alphas;
+                storage.chords = c';
+                storage.pitches = thetas;
                 storage.solidity = solidity;
                 storage.mass = mass;
                 storage.CD0 = cd;
                 storage.radius = r;
                 storage.rpm = v_tip / (2*pi*r) * 60;
                 success_blades = [success_blades, storage];
+            elseif L > thrust_per_blade
+                fail_check(3) = fail_check(3) + 1;
             end
         end
     end
