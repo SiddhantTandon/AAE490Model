514bd9066353a287e26a6b99106fda2fd03c87bc
Matt <mattwilson@Matts-MacBook.lfytina2.metronetinc.com>
2018-03-22 13:06:41 -0400

    Adjusted code with input from Tom Adams
    Tom Adams from Naval Surface Warfare Center - Crane Division, referred by Dr. Dietz

--- Propulsion Sizing/massBattery.m
+++ Propulsion Sizing/massBattery.m
@@ -14,17 +14,10 @@ function [ m_batt ] = massBattery( cap_batt, V_batt )
 %   Use a simple estimation mass of battery based on typical specific energy of lithium batteries  
 %   Use specific energy that is better than what is currently available to extrapolate available technology to the actual mission timeframe
 %   Battery experiences 0 degree C temperatures on average for mission lifespan
-% 
-% Notes:
-%   Li-Ion loses 2% Columbic Efficiency per month at 0 degrees C --> ~6% for 90 sols; source: http://batteryuniversity.com/learn/article/bu_808b_what_causes_li_ion_to_die 
-%   "Coulombic efficiency is capable of measuring both changes: the
-%   lithium lost due to SEI growth on the anode and electrolyte oxidation at the cathode. The results can be used to rank the life expectancy of a battery by quantifying the parasitic reaction."
 %     
 % Design Variables
     specific_energy = 350;    % [W*hr/kg]   (Typical range for lithium batteries: 100-250 Wh/kg); 350 is extrapolated for the year 2030
-    
-    cap_batt_eff = cap_batt / 0.94;  % accounts for 6% Columbic efficiency lost during mission lifespan
-    m_batt = cap_batt_eff * V_batt / specific_energy;
+    m_batt = cap_batt * V_batt / specific_energy;
     
 end
 
