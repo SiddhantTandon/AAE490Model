75721ae46592afe1b4c0e77c123b8c74dbc9810f
chris-brand <36180684+chris-brand@users.noreply.github.com>
2018-03-30 13:43:28 -0400

    Update Power_Forward_Flight.m

--- Propulsion Sizing/Power_Forward_Flight.m
+++ Propulsion Sizing/Power_Forward_Flight.m
@@ -1,4 +1,4 @@
-function [P_forward, cap_batt_forward_accel, P_mech_forward_accel, P_elec_forward_accel] = Power_Forward_Flight(weight, rho, radius, k, a, tipMach, Cdp, s, beta, V_inf, accel_forward, Idraw, V_batt, motor_eff, numProp)
+function [P_forward, cap_batt_forward_accel, P_mech_forward_accel, P_elec_forward_accel] = Power_Forward_Flight(weight, rho, radius, k, a, tipMach, Cdp, s, beta_cruise, beta_accel, V_inf, accel_forward, Idraw, V_batt, motor_eff, numProp)
 
 % Constants
 k_span_drag = 4.6; % factor due to spanwise drag, value taken from textbook Ch 5, pg 133
@@ -10,13 +10,16 @@ vehicle_mass = weight/3.71;
 
 
 % Calculate power and battery capacity required to accelerate
-V_inf_0 = 0.2; %m/s
+V_inf_0 = 0; %m/s
 V_inf_final = V_inf; %m/s
+t_accel = (V_inf_final - V_inf_0)/accel_forward;
+t_step = 0.1;
 i = 1;
-for V_inf = V_inf_0:0.2:V_inf_final
+for t = 0.1:t_step:t_accel
+    V_inf = t*accel_forward;
     % Calculate velocities at the rotor based on inclination angle (beta) of rotor disk
-    Vx = V_inf * cosd(beta);
-    Vz = V_inf * sind(beta);
+    Vx = V_inf * cosd(beta_accel);
+    Vz = V_inf * sind(beta_accel);
     Vtip = (tipMach * a)-Vx;
 
     % Calculate induced velocity based on weight and rotor inclination angle
@@ -30,8 +33,8 @@ for V_inf = V_inf_0:0.2:V_inf_final
 %     beta = asind(Tx/T_total);
 %     T= T_total/numProp;
 
-    beta = 20;
-    T = (weight / cosd(beta));
+   
+    T = (weight / cosd(beta_accel));
     lambda_i0 = sqrt(T / (2*rho*pi*radius^2)) / Vtip;
     ux = Vx / Vtip;
     uz = Vz / Vtip;
@@ -41,7 +44,7 @@ for V_inf = V_inf_0:0.2:V_inf_final
     lambda_i_bar = roots(p);
     lambda_i = lambda_i_bar * lambda_i0;
     vi = lambda_i * Vtip;
-    % vi_2 = (T / (2*rho*pi*radius^2)) * (1/sqrt(Vx^2 + (Vz+real(vi(4)))^2));
+    vi_2 = (T / (2*rho*pi*radius^2)) * (1/sqrt(Vx^2 + (Vz+real(vi(4)))^2));
     vi = real(vi(4));
 
     % Calculate power required
@@ -53,12 +56,62 @@ for V_inf = V_inf_0:0.2:V_inf_final
     
     P_mech_forward_accel(i) = numProp * P_forward(i);
     P_elec_forward_accel(i) = P_mech_forward_accel(i)/motor_eff;     % Convert from mechanical to electrical power
-    cap_batt_forward_accel(i) = capacityBattery( P_elec_forward_accel(i), V_batt, 0.2/3600, Idraw );
+    cap_batt_forward_accel(i) = capacityBattery( P_elec_forward_accel(i), V_batt, t_step/3600, Idraw );
     
     i = i+1;
 end
 
-cap_batt_forward_accel = sum(cap_batt_forward_accel); %A*hr
+% i = 1;
+% for t = 0:t_step:t_accel
+%     V_inf = V_inf_final - t*accel_forward;
+%     % Calculate velocities at the rotor based on inclination angle (beta) of rotor disk
+%     Vx = V_inf * cosd(-1 * beta_accel);
+%     Vz = V_inf * sind(-1 * beta_accel);
+%     Vtip = (tipMach * a)-Vx;
+% 
+%     % Calculate induced velocity based on weight and rotor inclination angle
+% %     C_D = 2;
+% %     A_body = 1;
+% %     D = 0.5*C_D*A_body*rho*V_inf^2;
+% %     W = vehicle_mass*3.71;
+% %     Tx = D + vehicle_mass*accel_forward;
+% %     Ty = W;
+% %     T_total = sqrt(Tx^2 + Ty^2);
+% %     beta = asind(Tx/T_total);
+% %     T= T_total/numProp;
+% 
+%    
+%     T = (weight / cosd(beta_accel));
+%     lambda_i0 = sqrt(T / (2*rho*pi*radius^2)) / Vtip;
+%     ux = Vx / Vtip;
+%     uz = Vz / Vtip;
+%     ux_bar = ux / lambda_i0;
+%     uz_bar = uz / lambda_i0;
+%     p = [1, 2*uz_bar, (ux_bar^2 + uz_bar^2), 0, -1];
+%     lambda_i_bar = roots(p);
+%     lambda_i = lambda_i_bar * lambda_i0;
+%     vi = lambda_i * Vtip;
+%     vi_2 = (T / (2*rho*pi*radius^2)) * (1/sqrt(Vx^2 + (Vz+real(vi(4)))^2));
+%     vi = real(vi(4));
+% 
+%     % Calculate power required
+%     P_1 = k*vi*T;
+%     P_2 = (1/8)*Cdp*rho*Ab*Vtip^3;
+%     P_3 = 1 + k_span_drag*(V_inf/Vtip)^2;
+%     P_4 = 0.5*rho*V_inf^3*f;
+%     P_forward_2(i) = (P_1 + P_2*P_3 + P_4);
+%     
+%     P_mech_forward_accel_2(i) = numProp * P_forward_2(i);
+%     P_elec_forward_accel_2(i) = P_mech_forward_accel_2(i)/motor_eff;     % Convert from mechanical to electrical power
+%     cap_batt_forward_accel_2(i) = capacityBattery( P_elec_forward_accel_2(i), V_batt, t_step/3600, Idraw );
+%     
+%     i = i+1;
+% end
+
+% cap_batt_forward_accel = 8 * (sum(cap_batt_forward_accel) + sum(capp_batt_forward_accel_2)); %A*hr
+% P_mech_forward_accel = max([max(P_mech_forward_accel), max(P_mech_forward_accel_2)]);
+% P_elec_forward_accel = max([max(P_elec_forward_accel), max(P_elec_forward_accel_2)]);
+cap_batt_forward_accel = 8 * sum(cap_batt_forward_accel); %A*hr
 P_mech_forward_accel = max(P_mech_forward_accel);
 P_elec_forward_accel = max(P_elec_forward_accel);
 t_accel = (V_inf_final - V_inf_0)/accel_forward; %s
@@ -66,8 +119,8 @@ dist_accel = 0.5*(V_inf_final + V_inf_0)*t_accel; %m
 
 
 % Calculate velocities at the rotor based on inclination angle (beta) of rotor disk
-Vx = V_inf * cosd(beta);
-Vz = V_inf * sind(beta);
+Vx = V_inf * cosd(beta_cruise);
+Vz = V_inf * sind(beta_cruise);
 Vtip = (tipMach * a)-Vx;
 
 % Calculate induced velocity based on weight and rotor inclination angle
@@ -81,8 +134,8 @@ Vtip = (tipMach * a)-Vx;
 % beta = asind(Tx/T_total);
 % T= T_total/numProp;
 
-beta = 2;
-T = weight / cosd(beta);
+
+T = weight / cosd(beta_cruise);
 
 lambda_i0 = sqrt(T / (2*rho*pi*radius^2)) / Vtip;
 ux = Vx / Vtip;
