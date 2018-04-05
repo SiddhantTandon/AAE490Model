7ec4cc1cc5879e1f997784bfcfb63cc715d17f66
chris-brand <36180684+chris-brand@users.noreply.github.com>
2018-04-05 12:30:40 -0400

    Update Power_Descend.m

--- Propulsion Sizing/Power_Descend.m
+++ Propulsion Sizing/Power_Descend.m
@@ -1,4 +1,4 @@
-function [P_descend, Time_descend_hr, t_descend_accel_hr] = Power_Descend(weight, rho, radius, a, tipMach, Cdp, s, Vdescend, h, A_body, Cd_body, numProp, accel_descend)
+function [P_descend, Time_descend_hr, t_descend_accel_hr] = Power_Descend(weight, rho, radius, a, tipMach, Cdp, s, Vdescend, h, A_body, Cd_body, numProp, accel_descend, P_hover)
 
 % Calculate Power to Descend
     Vdescend_final = Vdescend;
@@ -12,10 +12,14 @@ function [P_descend, Time_descend_hr, t_descend_accel_hr] = Power_Descend(weight
 
     Tdescend = weight - 0.5*rho*A_body*Vdescend^2*Cd_body/numProp; % subtract drag from weight to get descent thrust req
     v_i_0 = sqrt(Tdescend/(2*rho*pi*radius^2));
-    Vdescend = 2.01*v_i_0; %m/s
     
-    v_i_descend = Vdescend/2 + sqrt((Vdescend/2)^2 - v_i_0^2);
-    Vtip_descend = tipMach * a;
-    omega_descend = Vtip_descend / radius;
-    Pp_descend = (Cdp * s * rho / 8) * (omega_descend*radius)^3 * (pi*radius^2);
-    P_descend = 1.2*Tdescend*(Vdescend - v_i_descend) + Pp_descend;
+    if Vdescend_final >= (2*v_i_0)
+    
+        Vdescend = Vdescend_final;
+        v_i_descend = Vdescend/2 + sqrt((Vdescend/2)^2 - v_i_0^2);
+        Vtip_descend = tipMach * a;
+        omega_descend = Vtip_descend / radius;
+        Pp_descend = (Cdp * s * rho / 8) * (omega_descend*radius)^3 * (pi*radius^2);
+        P_descend = 1.2*Tdescend*(Vdescend - v_i_descend) + Pp_descend;
+    else
+        P_descend = P_hover;
