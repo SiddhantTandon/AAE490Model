1bb30dece0ceb015fea4654210d9fc04e7f961b4
aklupar <aidan.klupar@analyticalspace.com>
2018-10-11 01:12:41 -0400

    Added Q, E_tot functionality to motor function
    Calculates the heat dissipation and total energy required

--- Systems/Motor Function/Motor.m
+++ Systems/Motor Function/Motor.m
@@ -1,29 +1,38 @@
-function [ ] = Motor( )
+function [ Q_dot, E_req_flight ] = Motor( t )
 %% Description
 % Determines motor sizing and performance metrics
  
 %% Inputs
-
+% t            - Time spent in each flight regime [s]
+%                [Climb, Descend, Forward Flight, Hover]
 
 %% Outputs
+% Q_dot        - Rate of heat generation for each flight regime (W)
+%                [Climb, Descend, Forward Flight, Hover]
+% E_req_flight - Total amount of energy required for entire flight [J]
 
 %% Constants
 numProp = 4;
+motor_eff = 0.85; % Rough Estimation (Can be found experimentally)
 
 %% --- Begin Code ---
 %% Hover
 % Calculate Power to Hover
-[P_hover, ~] = Power_Hover(T, rho, r, w, Cdp, s);
+% These functions still need to be defined.
+P_climb   = Power_Climb();
+P_descend = Power_Descend();
+P_forward = Power_ForwardFlight();
+P_hover   = Power_Hover();
+
+P = [P_climb P_descend P_forward P_hover];
 
 % Mechanicla and Electrical Power 
-P_mech_hover = numProp * P_hover;                  
-P_elec_hover = P_mech_hover / motor_eff;
-dQ = P_elec_hover * (1 - motor_eff); % Assuming extra power is converted to heat.
+P_mech = numProp * P;                  
+P_elec = P_mech / motor_eff;
+Q_dot = P_elec * (1 - motor_eff); % Assuming all extra power is converted to heat.
 
-%% Climb
-
-%% Descent
-
+% Total Energy Required
+E_req_flight = dot(P_elec, t);
 
 %% --- End Code ---
 
